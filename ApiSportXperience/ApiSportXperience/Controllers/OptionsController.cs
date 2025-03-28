23c85e0d58b9f63cda202bba93959287f4519bad
oscargutierrez <oscar_gutierrezrodriguez@iescarlesvallbona.cat>
2025-03-28 17:42:54 +0100

    Last changes

---
+++ ApiSportXperience/ApiSportXperience/Controllers/OptionsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiSportXperience.Models;
+
+namespace ApiSportXperience.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class OptionsController : ControllerBase
+    {
+        private readonly SportXperienceContext _context;
+
+        public OptionsController(SportXperienceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Options
+        [HttpGet]
+        [Route("api/options/products/{id}")]
+        public async Task<ActionResult<IEnumerable<Option>>> GetOptionsByProduct(int id)
+        {
+            return await _context.Options.Where(x => x.ProductId == id).ToListAsync();
+        }
+
+        // GET: api/Options/5
+        [HttpGet]
+        [Route("api/options/{id}")]
+        public async Task<ActionResult<Option>> GetOption(int id)
+        {
+            var option = await _context.Options.FindAsync(id);
+
+            if (option == null)
+            {
+                return NotFound();
+            }
+
+            return option;
+        }
+
+        // PUT: api/Options/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        [Route("api/options/{id}")]
+        public async Task<IActionResult> PutOption(int id, [FromBody] Option option)
+        {
+            if (id != option.OptionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(option).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OptionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Options
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("api/options")]
+        public async Task<ActionResult<Option>> PostOption( [FromBody] Option option)
+        {
+            _context.Options.Add(option);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOption", new { id = option.OptionId }, option);
+        }
+
+        // DELETE: api/Options/5
+        [HttpDelete]
+        [Route("api/options/{id}")]
+        public async Task<IActionResult> DeleteOption(int id)
+        {
+            var option = await _context.Options.FindAsync(id);
+            if (option == null)
+            {
+                return NotFound();
+            }
+
+            _context.Options.Remove(option);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OptionExists(int id)
+        {
+            return _context.Options.Any(e => e.OptionId == id);
+        }
+    }
+}
---
+++ ApiSportXperience/ApiSportXperience/Controllers/ResultsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiSportXperience.Models;
+
+namespace ApiSportXperience.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class ResultsController : ControllerBase
+    {
+        private readonly SportXperienceContext _context;
+
+        public ResultsController(SportXperienceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Results
+        [HttpGet]
+        [Route("api/results/events/{id}")]
+        public async Task<ActionResult<IEnumerable<Result>>> GetResultsByEvent(int eventId)
+        {
+            return await _context.Results.Where(x => x.EventId == eventId).ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("api/results/users/{id}")]
+        public async Task<ActionResult<IEnumerable<Result>>> GetResultsByUser(string userDni)
+        {
+            return await _context.Results.Where(x => x.UserDni.Equals(userDni)).ToListAsync();
+        }
+
+        // GET: api/Results/5
+        [HttpGet]
+        [Route("api/results/{id}")]
+        public async Task<ActionResult<Result>> GetResult(int id)
+        {
+            var result = await _context.Results.FindAsync(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return result;
+        }
+
+        // PUT: api/Results/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        [Route("api/results/{id}")]
+        public async Task<IActionResult> PutResult(int id, [FromBody] Result result)
+        {
+            if (id != result.ResultId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(result).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ResultExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Results
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("api/results")]
+        public async Task<ActionResult<Result>> PostResult( [FromBody] Result result)
+        {
+            _context.Results.Add(result);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetResult", new { id = result.ResultId }, result);
+        }
+
+        // DELETE: api/Results/5
+        [HttpDelete]
+        [Route("api/results/{id}")]
+        public async Task<IActionResult> DeleteResult(int id)
+        {
+            var result = await _context.Results.FindAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            _context.Results.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ResultExists(int id)
+        {
+            return _context.Results.Any(e => e.ResultId == id);
+        }
+    }
+}
