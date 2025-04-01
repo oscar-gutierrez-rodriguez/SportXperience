7b35a316714d17d10dffd556e91533962b132aab
oscargutierrez <oscar_gutierrezrodriguez@iescarlesvallbona.cat>
2025-04-01 19:23:36 +0200

    last changes

---
+++ ApiSportXperience/ApiSportXperience/Controllers/ParticipantsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiSportXperience.Models;
+
+namespace ApiSportXperience.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class ParticipantsController : ControllerBase
+    {
+        private readonly SportXperienceContext _context;
+
+        public ParticipantsController(SportXperienceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Participants
+        [HttpGet]
+        [Route("api/participants/{eventId:int}")]
+        public async Task<ActionResult<IEnumerable<Participant>>> GetParticipantsByEvent(int eventId)
+        {
+            return await _context.Participants.Where(x => x.EventId == eventId).ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("api/participants/organizer/{eventId}")]
+        public async Task<ActionResult<Participant>> GetOrganizerByEvent(int eventId)
+        {
+            return await _context.Participants.Where(x => x.EventId == eventId && x.Organizer == true).FirstOrDefaultAsync();
+        }
+
+        // GET: api/Participants/5
+        [HttpGet]
+        [Route("api/participants/{id}")]
+        public async Task<ActionResult<Participant>> GetParticipantByDni(string dni)
+        {
+            var participant = await _context.Participants.Where(x => x.UserDni.Equals(dni)).FirstOrDefaultAsync();
+
+            if (participant == null)
+            {
+                return NotFound();
+            }
+
+            return participant;
+        }
+
+        // PUT: api/Participants/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        [Route("api/participants/{id}")]
+        public async Task<IActionResult> PutParticipant(int id, Participant participant)
+        {
+            if (id != participant.EventId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(participant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ParticipantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Participants
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("api/participants")]
+        public async Task<ActionResult<Participant>> PostParticipant(Participant participant)
+        {
+            _context.Participants.Add(participant);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ParticipantExists(participant.EventId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetParticipant", new { id = participant.EventId }, participant);
+        }
+
+        // DELETE: api/Participants/5
+        [HttpDelete]
+        [Route("api/participants/{eventId}/{userDni}")]
+        public async Task<IActionResult> DeleteParticipant(int eventId, string userDni)
+        {
+            var participant = await _context.Participants.Where(x => x.EventId == eventId && x.UserDni.Equals(userDni)).FirstOrDefaultAsync();
+            if (participant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Participants.Remove(participant);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ParticipantExists(int id)
+        {
+            return _context.Participants.Any(e => e.EventId == id);
+        }
+    }
+}
---
+++ ApiSportXperience/ApiSportXperience/Controllers/UbicationsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiSportXperience.Models;
+
+namespace ApiSportXperience.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class UbicationsController : ControllerBase
+    {
+        private readonly SportXperienceContext _context;
+
+        public UbicationsController(SportXperienceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ubications
+        [HttpGet]
+        [Route("api/ubications")]
+        public async Task<ActionResult<IEnumerable<Ubication>>> GetUbications()
+        {
+            return await _context.Ubications.ToListAsync();
+        }
+
+        // GET: api/Ubications/5
+        [HttpGet]
+        [Route("api/ubications/{id}")]
+        public async Task<ActionResult<Ubication>> GetUbication(int id)
+        {
+            var ubication = await _context.Ubications.FindAsync(id);
+
+            if (ubication == null)
+            {
+                return NotFound();
+            }
+
+            return ubication;
+        }
+
+        [HttpGet]
+        [Route("api/ubications/{latitude}/{longitude}")]
+        public async Task<ActionResult<Ubication>> GetUbicationByLatitudeLongitude(float latitude, float longitude)
+        {
+            var ubication = await _context.Ubications.Where(x => x.Latitude == latitude && x.Longitude == longitude).FirstOrDefaultAsync();
+
+            if (ubication == null)
+            {
+                return NotFound();
+            }
+
+            return ubication;
+        }
+
+        // PUT: api/Ubications/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        [Route("api/ubications/{id}")]
+        public async Task<IActionResult> PutUbication(int id, Ubication ubication)
+        {
+            if (id != ubication.UbicationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ubication).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UbicationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Ubications
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("api/ubications")]
+        public async Task<ActionResult<Ubication>> PostUbication(Ubication ubication)
+        {
+            _context.Ubications.Add(ubication);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUbication", new { id = ubication.UbicationId }, ubication);
+        }
+
+        // DELETE: api/Ubications/5
+        [HttpDelete]
+        [Route("api/ubications/{id}")]
+        public async Task<IActionResult> DeleteUbication(int id)
+        {
+            var ubication = await _context.Ubications.FindAsync(id);
+            if (ubication == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ubications.Remove(ubication);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UbicationExists(int id)
+        {
+            return _context.Ubications.Any(e => e.UbicationId == id);
+        }
+    }
+}
