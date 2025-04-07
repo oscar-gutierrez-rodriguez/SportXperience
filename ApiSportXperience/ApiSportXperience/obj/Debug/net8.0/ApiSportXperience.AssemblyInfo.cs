1dc05a0719411ba8cbfa502c989f9302c986df93
oscargutierrez <oscar_gutierrezrodriguez@iescarlesvallbona.cat>
2025-04-07 16:49:13 +0200

    last changes

--- ApiSportXperience/ApiSportXperience/obj/Debug/net8.0/ApiSportXperience.AssemblyInfo.cs
+++ ApiSportXperience/ApiSportXperience/obj/Debug/net8.0/ApiSportXperience.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiSportXperience")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68083c4e84aecf3ba3baa9483f8ff2b37e19aebd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debaf0dc4867ffeeeeb750f474a6ff5ba75050aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiSportXperience")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiSportXperience")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
